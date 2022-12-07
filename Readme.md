2ee8745d74e525553d2fa1d91394c38c35047301
davis <zanxiaofeng@163.com>
2022-12-07 10:57:29 +0800

    Development for snapshot 1.0.0

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
 # Please config maven setting file for idea
+
+
+# snapshot
+- mvn deploy -s ./settings.xml
+
+# release
+- mvn release:prepare
+- mvn release:perform -s ./settings.xml 
--- src/main/java/zxf/example/release/main/ReleaseMain.java
+++ src/main/java/zxf/example/release/main/ReleaseMain.java
@@ -5,6 +5,6 @@ import zxf.example.release.jar.ReleaseJar;
 public class ReleaseMain {
     public static void main(String[] args) {
         ReleaseJar.main();
-        System.out.println("release-main: v1.0.0-SNAPSHOT-1");
+        System.out.println("release-main: v1.0.0");
     }
 }
